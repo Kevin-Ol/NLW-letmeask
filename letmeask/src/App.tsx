d969d4025a76ce8656823d80607a748f222be60d
Kevin Oliveira <kevin.zero@hotmail.com>
2021-06-20 23:16:27 -0300

    Configuraçao inicial do projeto

---
+++ letmeask/src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from './Components/Button'
+
+function App() {
+  return (
+    // <h1>Hello Worldd</h1>
+    <div>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+    </div>
+  );
+}
+
+export default App;

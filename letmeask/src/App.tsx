0b72f33ba831142a1300bc439d3f3f3816f9a5ea
Kevin Oliveira <kevin.zero@hotmail.com>
2021-06-21 23:18:03 -0300

    Cria paginas principais

--- letmeask/src/App.tsx
+++ letmeask/src/App.tsx
@@ -1,14 +1,14 @@
-import { Button } from './Components/Button'
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import { Home } from './pages/Home';
+import { NewRoom } from "./pages/NewRoom";
 
 function App() {
   return (
-    // <h1>Hello Worldd</h1>
-    <div>
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
-    </div>
+    <BrowserRouter>
+      <Route path="/" exact component={Home} />
+      <Route path="/rooms/new" component={NewRoom} />
+    </BrowserRouter>
   );
 }
 
--- letmeask/src/index.tsx
+++ letmeask/src/index.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import './Services/firebase'
+import './services/firebase'
+
+import './styles/global.scss'
 
 ReactDOM.render(
   <React.StrictMode>

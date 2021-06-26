607d727c461f21af8b6f10d4c3dd09a4a8246f82
Kevin Oliveira <kevin.zero@hotmail.com>
2021-06-26 19:32:55 -0300

    Adiciona funcionalidades de excluir e destacar perguntas

--- letmeask/src/pages/Room.tsx
+++ letmeask/src/pages/Room.tsx
@@ -101,7 +101,10 @@ export function Room() {
                             key={question.id}
                             content={question.content}
                             author={question.author}
+                            isAnswered={question.isAnswered}
+                            isHighlighted={question.isHighlighted}
                             >
+                            { !question.isAnswered && (
                             <button
                                 className={`like-button ${question.likeId ? 'liked' : ''}`}
                                 type="button"
@@ -114,6 +117,7 @@ export function Room() {
                                 </svg>
 
                             </button>
+                            ) }
                             </Question>
                         );
                     })}
